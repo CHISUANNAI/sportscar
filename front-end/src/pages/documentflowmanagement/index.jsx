1e84b2353e772a0cdf380edee19203e3db971182
miaomiao <1343930405@qq.com>
2022-08-17 17:34:43 +0800

    Update index.jsx

--- front-end/src/pages/documentflowmanagement/index.jsx
+++ front-end/src/pages/documentflowmanagement/index.jsx
@@ -1,12 +1,35 @@
 import React, { Component } from "react";
 import './index.css';
-
+import { Tree } from 'antd';
 
 export default class documentflowmanagement extends Component {
   render() {
+    const dig = (path = '0', level = 3) => {
+      const list = [];
+    
+      for (let i = 0; i < 2; i += 1) {
+        const key = `${path}-${i}`;
+        const treeNode = {
+          title: key,
+          key,
+        };
+    
+        if (level > 0) {
+          treeNode.children = dig(key, level - 1);
+        }
+    
+        list.push(treeNode);
+      }
+    
+      return list;
+    };
+    
+    const treeData = dig();
     return (
+      
       <div>
-        <h1>documentflowmanagement</h1>
+
+        <Tree treeData={treeData} height={233} defaultExpandAll />
       </div>
     );
   }
